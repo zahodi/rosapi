f8bc8a543eb626e1e665af60aaf5f730d101572b
Valentin Gurmeza <valentin.gurmeza@emcconnected.com>
2017-02-03 17:41:31 -0800

    add ip remove and add functions

--- rosapi/ip.py
+++ rosapi/ip.py
@@ -12,13 +12,30 @@ class IP():
         b'/ip/address/print',
         b'?=address=' +
         self.ip_address])
-    print(ip_response)
-    if self.ip_address == ip_response[0][1]["address"]:
-      ipExists = True
-    return ipExists
+    if ip_response[0][0] == "!done":
+      ipExists = False
+      ip_id = ""
+    else:
+      if self.ip_address == ip_response[0][1]["address"]:
+        ipExists = True
+        ip_id = ip_response[0][1][".id"]
+      else:
+        ipExists = False
+        ip_id = ""
+    return ipExists, ip_id
 
-  def ip_address_remove(arg):
-    pass
+  def ip_address_remove(self, ip_id):
+    ip_id = ip_id
+    self.api.talk([
+        b'/ip/address/remove',
+        b'=.id=' + ip_id])
 
-    def ip_address_add(arg):
-      pass
+  def ip_address_add(self, interface_name, comment):
+    interface = interface_name
+    comment = comment
+
+    self.api.talk([
+        b'/ip/address/add',
+        b'=address=' + self.ip_address,
+        b'=interface=' + interface,
+        b'=comment=' + comment])
